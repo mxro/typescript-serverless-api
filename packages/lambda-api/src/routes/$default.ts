4cdd01765387fa30fa046477274796a1fe105af0
Max Rohde <maxrohde.public@gmail.com>
2022-01-04 11:29:35 +1100

    Update index page for API

--- packages/lambda-api/src/routes/$default.ts
+++ packages/lambda-api/src/routes/$default.ts
@@ -10,8 +10,24 @@ type ProxyHandler = Handler<APIGatewayProxyEventV2, APIGatewayProxyResultV2>;
 export const handler: ProxyHandler = async (event, context) => {
   return {
     statusCode: 201,
-    body: JSON.stringify({
-      message: 'Unknown endpoint',
-    }),
+    headers: {
+      'content-type': 'text/html',
+    },
+    body: `
+    <html>
+      <head>
+        <title>TypeScript Serverless API Example</title>
+      </head>
+      <body>
+        <h1>TypeScript Serverless API Example</h1>
+        <p>Try out some of the endpoints!</p>
+        <ul>
+          <li><a href='/echo'>/echo?message=[message]</a>
+          <li><a href='/user/1234'>/user/[id]</a>
+        </ul>
+        <p>For more information, see the blog post <a href='https://maxrohde.com/2022/01/04/serverless-api-with-typescript-on-aws/'>Serverless API with TypeScript on AWS<a></p>
+      </body>
+    </html>
+    `,
   };
 };
